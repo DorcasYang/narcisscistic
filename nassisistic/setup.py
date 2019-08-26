c029040f7b3421111031d344c9cc27c0cfe7231f
DorcasYang <54299084+DorcasYang@users.noreply.github.com>
2019-08-26 16:59:38 +0800

    Add files via upload

---
+++ nassisistic/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='nassisistic',
+    version='0.1',
+    author='DorcasYang',
+    description='check if a number is narssisistic',
+    package_dir='/Users/yangyijing/PycharmProjects/nassisistic/nassisistic'
+)
