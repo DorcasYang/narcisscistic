8385633edb256aeaea0511b0bd675e05a5b6825c
DorcasYang <54299084+DorcasYang@users.noreply.github.com>
2019-08-26 17:20:23 +0800

    Delete setup.py

--- nassisistic/setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name='nassisistic',
-    version='0.1',
-    author='DorcasYang',
-    description='check if a number is narssisistic',
-    package_dir='/Users/yangyijing/PycharmProjects/nassisistic/nassisistic'
-)
